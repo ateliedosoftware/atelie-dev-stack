02f03877ebb4a8b0c6c8fa32e7cdfd364b4b18a6
JuliaHPM <juliahammesmello@gmail.com>
2021-10-19 14:49:22 -0300

    modified:   frontend/src/styles/global.css "Altera css global"

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,34 @@
 <template>
-  <!--<Login /> -->
   <div>
-    <div id="nav">
+    <!--<div id="nav">
       <div>
+        <NavBar />
         <router-link to="/">Home</router-link>
         <router-link to="/login">Login</router-link>
       </div>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
 
 <script>
-import "./styles/global.css";
-//import Login from "./views/Login.vue";
+import './styles/global.css'
+//import NavBar from '@/components/NavBar.vue'
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    //Login,
+    //NavBar,
   },
   watch: {
     $route: {
       immediate: true,
       handler(to) {
-        document.title = to.meta.title || "IFRS";
+        document.title = to.meta.title || 'IFRS'
       },
     },
   },
-};
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home'
 import Login from '../views/Login.vue'
+import Dashboard from '../views/Dashboard.vue'
+import CadastroUser from '../views/CadastroUser.vue'
+//import store from "../store";
+
 
 Vue.use(VueRouter)
 
@@ -18,7 +22,26 @@ const routes = [{
         name: 'Login',
         component: Login,
         meta: {
-            title: 'IFRS - Login'
+            title: 'IFRS - Login',
+            guest: true
+        }
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: Dashboard,
+        meta: {
+            title: 'IFRS - Dashboard',
+            //requiresAuth: true
+        }
+    },
+    {
+        path: '/cadastro',
+        name: 'cadastroUser',
+        component: CadastroUser,
+        meta: {
+            title: 'IFRS - Cadastro Usuário',
+            guest: true
         }
     }
 ]
@@ -28,5 +51,31 @@ const router = new VueRouter({
     base: process.env.BASE_URL,
     routes
 })
+
+
+/*verificação de autenticacao
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (store.getters.isAuthenticated) {
+      next();
+      return;
+    }
+    next("/login");
+  } else {
+    next();
+  }
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.guest)) {
+    if (store.getters.isAuthenticated) {
+      next("/dashboard");
+      return;
+    }
+    next();
+  } else {
+    next();
+  }
+});*/
 
 export default router
