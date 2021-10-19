eff15b5c1916f8bedb0e3fdf4b6fcd80b8593fcb
JuliaHPM <juliahammesmello@gmail.com>
2021-10-19 14:44:41 -0300

    Adiciona components

---
+++ frontend/src/components/NavBarDashboard.vue
@@ -0,0 +1,143 @@
+<template>
+  <main>
+    <div class="containerNavbar">
+      <div class="navbar">
+        <div class="navcontent">
+          <div class="banner">
+            <router-link to="/" class="routerlink"
+              ><img
+                src="https://ifrs.edu.br/wp-content/uploads/2017/08/logo_horizontal_simplificado-012.png"
+                style="width:15vh"
+            /></router-link>
+          </div>
+          <!--https://ifrs.edu.br/wp-content/uploads/2019/09/horizontal-nova-04.png -->
+
+          <router-link to="/dashboard" class="navbarlink"
+            ><img
+              class="icon"
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAe0lEQVRIie2UUQrAIAxDw07n/Y+gB5k/ThxY1gQZKj4QP9pSSJsCOxEAJAB358USV3Ir0ShoC5XcyhP0xNy5l5E0jGkbpPL39G/jL5gZBNiDNreIaeDm1xkwukpGY3SVjMYwp9EkXRkYXaWVPsfuswFjNOnYMUaTjt2aZFkdmGlyTRNBAAAAAElFTkSuQmCC"
+            />Dashboard</router-link
+          >
+          <h5 style="margin-top:30px;" class="title-nav">Usuários</h5>
+          <router-link to="/cadastro" class="navbarlink"
+            ><img
+              class="icon"
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAACNklEQVRYhe3VT28NURgG8F8lJaESC8pnaAkSWlZVH0BaJBY+QHcNC2ks6dK/unwDGyuEXfkMRSpEooJaktCS3kbba3HOZCY3c2emN9ONeJKTM5l5zvs8Z85535f/KEc/pvESv+KYw3Xs22rx81hCq8P4ibNbKb4RhR5jBLviOIUn8ds6xusW75fu/EoBbypyfmBvnQampTsvw9PIvVangVcx6EgF7mjkztVpYDkG7avA3R25S90Ibevwfm0TMXq6WFNq4GOcj1WIkXAW6jTwKM6XKsS43LamFuzHd+Fspwp4VyPnm5C6tWJcONeWkGqjwqXsw2k8i9/WMFa3eIJzQqXrVIrXIwcO4R7e4bfQM97gDga7ET+DDwXiyVjAgxKja2hgexXhHtzILH6NSQwIfWAPjgol+n2Gt4IZDEl7xjDuohk5z6uYuBnJTUzonClwP3K/CEfQCYexGLmNIvGxjHhZGT4odMuVHPHkr7SbaArHMZAXcAc+xYUTJeKEC9cSLlk78gwQdt/C7byAF6RnXvTbE7yN/OObMDAc38/nBXwYP05WECekWbZhlWULOY0ru9OhOL+oaGCjIq8QWQMH4rxYcW3CSy5UT2bo8C4pSJ/zDOyMxOWKBmbjfLEiP8udLWRVxKCQUk0hxbLIu4RHsKogDbtBklaLOSbaxb9G7kxd4tArlNekeDVwQto1Twr1YlVainvrNECo7Q1p684bf4Sd1y6exaBQ4eaFi7wcn2+p8cz/PfwFcFG+SI46/aEAAAAASUVORK5CYII="
+            />Cadastro</router-link
+          >
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  name: 'NavBarDashboard',
+  computed: {
+    isLoggedIn: function() {
+      return this.$store.getters.isAuthenticated
+    },
+  },
+  methods: {
+    async logout() {
+      await this.$store.dispatch('LogOut')
+      this.$router.push('/login')
+    },
+  },
+}
+</script>
+<style>
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+a:hover {
+  cursor: pointer;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.containerNavbar {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.navbar {
+  position: relative;
+  width: 15%;
+  height: 100%;
+  background-color: #f9fbfc;
+  color: rgb(65, 65, 65);
+  float: left;
+}
+
+.navcontent {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.navbarlink {
+  margin: 0;
+  padding: 0;
+  display: block;
+}
+
+.icon {
+  display: inline-block;
+  font-size: inherit;
+  height: 1.4em;
+  overflow: visible;
+  vertical-align: -0.125em;
+  margin-right: 5px;
+}
+
+.routerlink {
+  margin: 0;
+}
+
+.banner {
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+@media screen and (max-width: 1000px) {
+  .containerNavbar {
+    position: sticky;
+  }
+
+  .navbar {
+    width: 100%;
+    height: 100px;
+    background-color: inherit;
+    color: rgb(65, 65, 65);
+    display: flex;
+    padding: 0;
+  }
+
+  .navcontent {
+    margin-top: 0px;
+    width: 100%;
+    height: 100px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: left;
+  }
+
+  .title-nav {
+    display: none;
+  }
+
+  .navbarlink {
+    padding-right: 20px;
+    padding-left: 20px;
+  }
+}
+</style>
---
+++ frontend/src/components/TableUsers.vue
@@ -0,0 +1,355 @@
+<template>
+  <!--https://art-of-engineer.blogspot.com/2021/06/vue-js-crud-app.html
+https://github.com/ArtOfEngineer/Vue-Js-ASP-NET-Core-WebAPI/blob/main/ui/department.js
+https://www.youtube.com/watch?v=Jg4GOPaE4do&ab_channel=ArtofEngineer
+ -->
+  <div class="container">
+    <div class="row justify-content-between ">
+      <div class="col-8">
+        <div class="title-tab">
+          <h3>Usuários cadastrados</h3>
+        </div>
+        <!-- botão cadastrar usuario 
+        <router-link to="/cadastro"
+          ><a class="btn">
+            
+            Add
+          </a></router-link
+        >--><!--data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            @click="addClick()" -->
+      </div>
+
+      <!-- pesquisa -->
+      <div class="col-4 ">
+        <input
+          class="form-control input-pesquisa"
+          v-model="usernameFilter"
+          placeholder="Pesquisa"
+        />
+      </div>
+    </div>
+
+    <div class="table-scroll">
+      <table id="tableUsers" class="table table-sm">
+        <thead>
+          <tr>
+            <th>
+              <div class="d-flex flex-row align-items-end">
+                <div class="col-2 ">ID</div>
+                <!--<div class="col-10">
+                  <a @click="sortResult('userId', true)">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-arrow-down-square-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"
+                      />
+                    </svg>
+                  </a>
+                  <a @click="sortResult('userId', false)">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-arrow-up-square-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"
+                      />
+                    </svg>
+                  </a>
+                </div>-->
+              </div>
+            </th>
+            <th>
+              <!--mudar ordem alfabética username-->
+              <div class="d-flex flex-row align-items-end">
+                <div class="col-4 ">Usuário</div>
+                <div class="col-8">
+                  <a @click="sortResult('username', true)">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-arrow-down-square-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"
+                      />
+                    </svg>
+                  </a>
+
+                  <a @click="sortResult('username', false)">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-arrow-up-square-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </th>
+            <th>
+              <!--mudar ordem alfabética nome completo-->
+              <div class="d-flex flex-row align-items-end">
+                <div class="col-4 ">Nome</div>
+                <div class="col-8">
+                  <a @click="sortResult('fullname', true)">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-arrow-down-square-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"
+                      />
+                    </svg>
+                  </a>
+
+                  <a @click="sortResult('fullname', false)">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-arrow-up-square-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in resultQuery" :key="user.id">
+            <td style="text-align: center;">{{ user.id }}</td>
+            <td>{{ user.username }}</td>
+            <td>{{ user.fullname }}</td>
+            <td style="text-align: center; vertical-align: middle">
+              <button
+                type="button"
+                class="btn btn-light mr-1 mt-0"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                @click="editClick(user)"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-pencil-square"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
+                  />
+                </svg>
+              </button>
+
+              <button type="button" @click="deleteClick(user.id)" class="btn btn-light mr-1 mt-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-trash-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+                  />
+                </svg>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- modal editar ou criar usuario-->
+    <!--<div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{ modalTitle }}</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+
+          <div class="modal-body">
+            <div class="input-group mb-3">
+              <span class="input-group-text">Usuário</span>
+              <input type="text" class="form-control" v-model="username" />
+            </div>
+
+            <button type="button" @click="createClick()" v-if="userId == 0" class="btn btn-primary">
+              Create
+            </button>
+            <button type="button" @click="updateClick()" v-if="userId != 0" class="btn btn-primary">
+              Update
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>-->
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Dashboard',
+  components: {},
+  data() {
+    return {
+      users: [],
+      modalTitle: '',
+      username: '',
+      fullname: '',
+      userId: 0,
+      usernameFilter: null,
+      userIdFilter: '',
+      usersWithoutFilter: [],
+    }
+  },
+  computed: {
+    //pesquisa
+    resultQuery() {
+      if (this.usernameFilter != null) {
+        return this.users.filter(user => {
+          return (
+            this.usernameFilter
+              .toLowerCase()
+              .split(' ')
+              .every(v => user.username.toLowerCase().includes(v)) ||
+            this.usernameFilter
+              .toLowerCase()
+              .split(' ')
+              .every(v => user.fullname.toLowerCase().includes(v))
+          )
+        })
+      } else {
+        return this.users
+      }
+    },
+  },
+
+  //pegar os dados do json server
+  async created() {
+    const { data } = await axios.get('http://localhost:3000/users')
+    this.users = data
+  },
+  methods: {
+    refreshData() {
+      axios.get('http://localhost:3000/users').then(response => {
+        this.users = response.data
+        this.usersWithoutFilter = response.data
+      })
+    },
+    /*addClick() {
+      this.modalTitle = 'Cadastrar usuário'
+      this.userId = 0
+      this.username = ''
+      this.fullname = ''
+    },
+    editClick(user) {
+      this.modalTitle = 'Editar usuário'
+      this.userId = user.userId
+      this.username = user.username
+    },
+    async updateClick() {
+      await axios
+        .put('http://localhost:3000/users', {
+          username: this.username,
+          fullname: this.fullname,
+          password: this.password,
+        })
+        .then(response => {
+          this.refreshData()
+          alert(response.data)
+        })
+    },*/
+    deleteClick(id) {
+      if (!confirm('Excluir o usuário ' + id + '?')) {
+        return
+      }
+      axios.delete('http://localhost:3000/users/' + id).then(response => {
+        this.refreshData()
+        console.log(response.data)
+      })
+    },
+
+    sortResult(prop, asc) {
+      this.users = this.usersWithoutFilter.sort(function(a, b) {
+        if (asc) {
+          return a[prop] > b[prop] ? 1 : a[prop] < b[prop] ? -1 : 0
+        } else {
+          return b[prop] > a[prop] ? 1 : b[prop] < a[prop] ? -1 : 0
+        }
+      })
+    },
+  },
+  mounted: function() {
+    this.refreshData()
+  },
+}
+</script>
+
+<style scoped>
+.btn {
+  margin: 15px 0px;
+}
+
+.input-pesquisa {
+  margin: 15px 0;
+}
+
+.title-tab {
+  padding-top: 15px;
+}
+
+.table-scroll {
+  overflow-x: auto;
+}
+</style>
